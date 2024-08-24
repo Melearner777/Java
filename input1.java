601c0cee181327379c2365a0fae6ffba829ab64d
MD Jishan <152049778+Melearner777@users.noreply.github.com>
2024-08-24 19:53:48 +0530

    Add files via upload

---
+++ input1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class input1 {
+    public static void main(String args[]){
+        Scanner x= new Scanner(System.in);
+        System.out.print("Enter your age:");
+        int age=x.nextInt();
+        System.out.println("Your age is:"+age);
+
+    }    
+}
